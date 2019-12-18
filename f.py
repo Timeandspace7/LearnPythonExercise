25f5815429b7964889d71e7ad804c879e8d99d68
Timeandspace7 <Timeandspace7@163.com>
2019-12-18 15:20:39 +0800

    f function

---
+++ f.py
@@ -0,0 +1,10 @@
+# by :T7
+def total_of_three():
+	num1 = input("Enter the first number:");
+	num2 = input("Enter the second number:");
+	num3 = input("Enter the third number:");
+	result = int(num1) + int(num2) + int(num3);
+	print(f"{num1} + {num2} + {num3} = {result}" );
+
+		
+total_of_three();
